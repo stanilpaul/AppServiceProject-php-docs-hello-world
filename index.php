87b05b55d844541ff737370e10a52478429de972
paul <paulstanil@gmail.com>
2025-07-15 19:02:44 +0200

    update

--- index.php
+++ index.php
@@ -3,4 +3,4 @@
 echo "Hello World! <br>
 Hello friends , How are you? <br> this is how my one of the app service project going ";
 
-echo "<h1>JUST FOR TESTING</h1>"
+echo "<h1>JUST FOR TESTING</h1>";
