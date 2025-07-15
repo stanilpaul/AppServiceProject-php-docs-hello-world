3d19abdac9787c493162faf8f477c82df1e9a43f
paul <paulstanil@gmail.com>
2025-07-15 19:06:34 +0200

    h1

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 
-echo "Hello World! <br>
-Hello friends , How are you? <br> this is how my one of the app service project going ";
+echo "<h1>Hello World! <br>
+Hello friends , How are you? <br> this is how my one of the app service project going</h1> ";
